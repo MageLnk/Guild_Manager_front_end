64d984a5ef26d0255a5522fad5b3328b1c1a1909
MageLink <mage.link.zhezard@gmail.com>
2022-07-19 21:37:11 -0400

    El login, en teoría debería funcionar, así que ahora toca empezar a hacer la logica si el login está bien o mal, para proceder a logear o no.

--- src/Api/index.js
+++ src/Api/index.js
@@ -1,13 +1,11 @@
 const apiCall = async ({ url, method = "get", body, headers }) => {
   try {
     const response = await fetch(url, { method, body, headers });
-    console.log("Response??", response);
     const data = await response.json();
-    console.log("Data??", data);
     return data;
   } catch (error) {
-    //Promise.reject(error);
-    console.log("Error dentro de la función apiCall", error);
+    Promise.reject(error);
+    //console.log("Error dentro de la función apiCall", error);
   }
 };
 
